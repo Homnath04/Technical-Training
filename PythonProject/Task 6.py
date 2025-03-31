87e32e50f0e11d6998f98516327b9afd48b02721
Homnath04 <homnathps22@gmail.com>
2025-03-31 12:43:43 +0530

    Add files via upload

---
+++ PythonProject/Task 6.py
@@ -0,0 +1,6 @@
+a = int(input('Enter value a'))
+b = int(input('Enter value b'))
+if a is b :
+    print('Both values share same memory location')
+else:
+    print('Both values have different memory location')
