9c388c66952237895d7efdb0173844fd96fcc83e
Homnath04 <homnathps22@gmail.com>
2025-03-31 12:48:33 +0530

    Add files via upload

---
+++ Daily Task 31-3/Task 2.py
@@ -0,0 +1,3 @@
+side = float(input('Enter value'))
+area = 6 * (side ** 2)
+print('Area of cube is', area)
---
+++ Daily Task 31-3/Task 3.py
@@ -0,0 +1,5 @@
+from math import *
+radius = float(input('Enter radius'))
+height = float(input('Enter height'))
+area = 2 * pi * radius * (radius+height)
+print('Area of Cylinder' , area)
